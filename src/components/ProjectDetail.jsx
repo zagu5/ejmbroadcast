a56e4d2bde0260317fea05fdaf8cdc1c059aa859
zagu5 <zury.agudelo@gmail.com>
2023-11-03 23:19:30 -0500

    carousel

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -3,18 +3,52 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import Footer from './Footer';
 import style from '../styles/projectDetail.module.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 const ProjectDetail = ({ projectData }) => {
+  const carouselItems = projectData.carouselImages.map((image, index) => (
+    <img key={index} src={image} alt={`Carousel Image ${index}`} />
+  ));
+
   return (
     <>
         <Header/>
         <div className={style.content}>
         <h2 className={style.title}>{projectData.title}</h2>
-        <img src={projectData.image} alt="cfc" />
- 
-        
+        <img src={projectData.image} alt="cfc" />        
+        <div className={style.row}>
+          <div className={style.column}>
+            <div className={style.location}>
+              <strong>Locación</strong>
+              <br />{projectData.location}
+            </div>
+          </div>
+          <div className={style.column}>
+            <div className={style.capacity}>
+              <strong>Capacidad</strong>
+              <br />{projectData.capacity}
+            </div>
+          </div>
+          <div className={`${style.column} ${style.lastColumn}`}>
+            <div className={style.services}>
+              <strong>Servicios</strong>
+              <br />{projectData.services}
+            </div>
+          </div>
+        </div>
         <p className={style.description}>{projectData.description}</p>
         </div>
+        <AliceCarousel 
+          items={carouselItems}
+          responsive={{
+          0: { items: 1 },
+          768: { items: 2 },
+          1024: { items: 3 },
+          }}
+          autoPlay
+          autoPlayInterval={3000}
+        />
         <Footer/>
     </>
 
@@ -26,6 +60,10 @@ ProjectDetail.propTypes = {
     title: PropTypes.string,
     description: PropTypes.string,
     image: PropTypes.string,
+    location: PropTypes.string,
+    capacity: PropTypes.string,
+    services: PropTypes.string,
+    carouselImages: PropTypes.arrayOf(PropTypes.string),
   }).isRequired,
 };
 
--- src/components/projectData.jsx
+++ src/components/projectData.jsx
@@ -3,36 +3,57 @@ export const projectData = {
     'project1': {
       title: 'Centro Familiar Cristiano',
       description: 'En una ciudad donde las condiciones son muy limitadas logramos trabajar en conjunto con un grupo de arquitectos e ingenieros para ',
-      image: '/public/Header_cfc.png',  
+      image: '/imagesHeaders/Header_cfc.png',  
+      location: 'Sincelejo, Colombia',
+      capacity: '2000 personas',
+      services: 'Participación en la construcción del auditorio',
+      carouselImages: [
+        '/carousel/Foto_mini_1.png',
+        '/carousel/Foto_mini_2.png',
+        '/carousel/Foto_mini_3.png',
+        '/carousel/Foto_mini_4.png',
+      ]
     },
     'project2': {
       title: '90 Minutos',
       description: 'Descripción del Proyecto 2',
-      image: '/public/Header_90minutos.png',
-
+      image: '/imagesHeaders/Header_90minutos.png',
+      location: 'Cali, Colombia',
+      capacity: '',
+      services: 'Participación en la construcción del centro de transición del noticiero para la Universidad Autónoma de Occidente',
     },
     'project3': {
       title: 'El Lugar de su Presencia',
       description: 'Descripción del Proyecto 3',
-      image: '/public/Header_elp.png',
-
+      image: '/imagesHeaders/Header_elp.png',
+      location: 'Bogotá, Colombia',
+      capacity: '2000 personas',
+      services: 'Suministro de equipos de video, audio e iluminación',
     },
     'project4': {
       title: 'Misión Carismática',
       description: 'Descripción del Proyecto 4',
-      image: '/public/Header_mci.png',
-
+      image: '/imagesHeaders/Header_mci.png',
+      location: 'Bogotá, Colombia',
+      capacity: '',
+      services: 'Suministro de equipos de video, audio e iluminación',
     },
     'project5': {
       title: 'Comunidad Cristiana Agua Viva',
       description: 'Descripción del Proyecto 5',
-      image: '/public/Header_ccav.png',
+      image: '/imagesHeaders/Header_ccav.png',
+      location: 'Lima, Perú',
+      capacity: '',
+      services: 'Suministro de equipos de video, audio e iluminación',
 
     },
     'project6': {
       title: 'Revista Semana',
       description: 'Descripción del Proyecto 6',
-      image: '/public/Header_revistasemana.png',
+      image: '/imagesHeaders/Header_revistasemana.png',
+      location: 'Colombia',
+      capacity: '',
+      services: 'Dotación e implementación del sistema audiovisual para su propuesta virtual informativa ',
 
     },
 
