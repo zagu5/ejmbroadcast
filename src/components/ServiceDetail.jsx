d7e6e266264f419348421c6761af559738a5ec35
zagu5 <zury.agudelo@gmail.com>
2023-11-09 10:30:26 -0500

    servidata complete with styles

--- src/components/ServiceDetail.jsx
+++ src/components/ServiceDetail.jsx
@@ -3,8 +3,17 @@ import Header from "./Header"
 import Footer from "./Footer"
 import WhatsAppButton from "./WhatsAppButton"
 import style from "../styles/serviceDetail.module.css"
+import React from 'react';
 
 export const ServiceDetail = ({serviceData}) => {
+  const TextWithBreaks = ({text}) => {
+    return text.split('\n').map((str, index) => (
+      <React.Fragment key={index}>
+        {str}
+        <br/>
+      </React.Fragment>
+    ));
+  }
   return (
     <>
         <Header/>
@@ -12,7 +21,20 @@ export const ServiceDetail = ({serviceData}) => {
         <h2 className={style.title}>{serviceData.title}</h2>
         <img src={serviceData.image} alt="" />
         </div>
+        <div className={style.infoContainer}>
+        {serviceData.information.map((info, index) => (
+          <div key={index} className={style.textContainer}>
+            <img src={info.image} alt="" />
+            <div className={style.textSection}>
+              <h3 className={style.infoTitle}><TextWithBreaks text={info.title} /></h3>
+              <h4 className={style.infoSubtitle}><TextWithBreaks text={info.subtitle} /></h4>
+              <p className={style.infoParagraph}>{info.paragraph}</p>
+            </div>
 
+          </div>
+        ))}
+        <button className={style.button}>Cotiza tu proyecto</button>
+        </div>
         <Footer/>
         <WhatsAppButton/>
     </>
@@ -23,6 +45,14 @@ ServiceDetail.propTypes = {
   serviceData: PropTypes.shape({
     title: PropTypes.string,
     image: PropTypes.string,
+    information: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        paragraph: PropTypes.string,
+        image: PropTypes.string,
+      })
+    ),
   }).isRequired,
 };
 
--- src/components/serviceData.jsx
+++ src/components/serviceData.jsx
@@ -4,6 +4,58 @@ export const serviceData =  {
         id:'1',
         title: 'ASESORIA EN DISEÑO Y CONSTRUCCIÓN',
         image: '/imagesHeaders/Header_servicios.png',
+        information: [
+            {
+                title: 'LO ACOMPAÑAMOS EN TODAS \n'+
+                       'LAS ETAPAS DE SU PROYECTO',
+                subtitle: 'Desde el diseño hasta la construcción de auditorios, teatros, iglesias, ' +
+                          'salones de conferencias y departamentos de producción; así como en ' +
+                          'la planeación, montaje y ejecución de eventos como shows en vivo, ' +
+                          'servicios religiosos, conciertos, etc., sin dejar pasar ningún detalle. \n' +
+                          '\n Contamos con un equipo técnico de primera línea en cada país, con '+
+                          'gran experiencia brindando soluciones en proyectos destacados ' +
+                          'nacional e internacionalmente. ',
+                paragraph: 'Somos el camino más seguro para hacer sus sueños realidad.',
+                image:'/Foto.png'
+            }
+        ]
     },
+
+    'venta': {
+        id:'2',
+        title: 'VENTA DE EQUIPOS DE BROADCAST',
+        image: '/imagesHeaders/Header_servicios.png',
+        information: [
+            {
+                title: 'RECIBA LOS MEJORES \n'+
+                       'PRECIOS DEL \n '+
+                       'MERCADO',
+                subtitle: 'Cuente con el beneficio exclusivo para nuestros clientes de asistencia ' +
+                          'directa por parte de los principales fabricantes de equipos de audio, ' +
+                          'video y proyección. Gracias a la relación que por años hemos ' +
+                          'construido con las grandes maras del mundo del Broadcast.',
+                paragraph: ' ',
+                image:'/Foto.png'
+            }
+        ]
+    },
+
+    'talleres': {
+        id:'2',
+        title: 'TALLERES Y SEMINARIOS',
+        image: '/imagesHeaders/Header_servicios.png',
+        information: [
+            {
+                title: 'SOMOS UNA FUENTE  \n'+
+                       'CONFIABLE DE CONSULTA',
+                subtitle: 'Para entender la utilidad de cada recurso y su razón para utilizarlo ' +
+                          'nuestro objetivo es promover el uso correcto de los equipos de audio, ' +
+                          'video y proyección de acuerdo a los parámetros científicos ' +
+                          'desarrollados por nuestra compañia en diferentes experiencias.',
+                paragraph: ' ',
+                image:'/Foto.png'
+            }
+        ]
+    }
 }
 
