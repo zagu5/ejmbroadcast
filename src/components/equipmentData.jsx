11befa7c53d59cd6535fcb8a110478e8479c8069
zagu5 <zury.agudelo@gmail.com>
2023-12-11 19:37:50 -0500

    update brands equipment page

--- src/components/equipmentData.jsx
+++ src/components/equipmentData.jsx
@@ -5,47 +5,53 @@ import audioIcon from '../assets/images/equipment/audioIcon.svg';
 import softwareIcon from '../assets/images/equipment/softwareIcon.svg';
 import headerSoftware from '../assets/images/equipment/Header_software.png';
 
-import sony from '../assets/images/brands/sony.png';
-import datavideo from '../assets/images/brands/datavideo.png';
-import panasonic from '../assets/images/brands/panasonic.png';
-import canon from '../assets/images/brands/Canon.png';
-import red from '../assets/images/brands/Red.png';
-import newtek from '../assets/images/brands/Newtek.png';
-import birddog from '../assets/images/brands/BirdDog.png';
-import jvc from '../assets/images/brands/JVC.png';
-import aidaimaging from '../assets/images/brands/Aida.png';
-import ptzoptics from '../assets/images/brands/PTZOptics.png';
-import blackmagic from '../assets/images/brands/Black_Magic.png';
-import fujifilm from '../assets/images/brands/Fujifilm.png';
-import teradek from '../assets/images/brands/Teradek.png';
-import manfrotto from '../assets/images/brands/Manfrotto.png';
-import acebil from '../assets/images/brands/Acebil.png';
-import roland from '../assets/images/brands/Roland.png';
-import aja from '../assets/images/brands/Aja.png';
-import canare from '../assets/images/brands/Canare.png';
-import magewell from '../assets/images/brands/Magewell.png';
-import nanlite from '../assets/images/brands/Nanlite.png';
-import atomos from '../assets/images/brands/Atomos.png';
-import angelbird from '../assets/images/brands/Angelbird.png';
-import eartec from '../assets/images/brands/Eartec.png';
-import unilumin from '../assets/images/brands/Unilumin.png';
-import lacie from '../assets/images/brands/Lacie.png';
-import swit from '../assets/images/brands/Swit.png';
-import shure from '../assets/images/brands/Shure.png';
-import bose from '../assets/images/brands/Bose.png';
-import azden from '../assets/images/brands/Azden.png';
-import resi from '../assets/images/brands/Resi.png';
-import wowza from '../assets/images/brands/Wowza.png';
-import vegas from '../assets/images/brands/Vegas.png';
-import telestream from '../assets/images/brands/Telestream.png';
-import magix from '../assets/images/brands/Magix.png';
-import reckreen from '../assets/images/brands/Reckreen.png';
-import softron from '../assets/images/brands/Softron.png';
-import yololiv from '../assets/images/brands/Yololiv.png';
-import iconvideo from '../assets/images/brands/iconvideo.svg';
-import iconaccesorios from '../assets/images/brands/iconaccesorios.svg';
-import iconaudio from '../assets/images/brands/iconaudio.svg';
-import iconsoftware from '../assets/images/brands/iconsoftware.svg';
+import panasonic from '../assets/images/brands/Panasonic_Fotografia.png';
+import sony from '../assets/images/brands/Sony_Fotografia.png';
+import canon from '../assets/images/brands/Canon_Fotografia.png';
+import datavideo from '../assets/images/brands/Datavideo_Fotografia.png';
+import red from '../assets/images/brands/Red_Fotografia.png';
+import newtek from '../assets/images/brands/NewTek_Fotografia.png';
+import birddog from '../assets/images/brands/BirdDog_Fotografia.png';
+import jvc from '../assets/images/brands/JVC_Fotografia.png';
+import aidaimaging from '../assets/images/brands/Aida_Fotografia.png';
+import ptzoptics from '../assets/images/brands/PTZOptics_Fotografia.png';
+// import fujifilm from '../assets/images/brands/Fujifilm_Fotografia.png';
+import sony_accesorios from '../assets/images/brands/Sony_Accesorios.png';
+import canon_accesorios from '../assets/images/brands/Canon_Accesorios.png';
+import blacmagic_accesorios from '../assets/images/brands/Blackmagic_Accesorios.png';
+import fujifilm_accesorios from '../assets/images/brands/Fujifilm_Accesorios.png';
+import teradek_accesorios from '../assets/images/brands/Teradek_Accesorios.png';
+import manfrotto_accesorios from '../assets/images/brands/Manfrotto_Accesorios.png';
+import aceail_accesorios from '../assets/images/brands/acebil_Accesorios.png';
+import roland_accesorios from '../assets/images/brands/Roland_Accesorios.png';
+import aja_accesorios from '../assets/images/brands/Aja_Accesorios.png';
+import canare_accesorios from '../assets/images/brands/Canare_Accesorios.png';
+import magewell_accesorios from '../assets/images/brands/Magewell_Accesorios.png';
+import nanlite_accesorios from '../assets/images/brands/Nanlite_Accesorios.png';
+import atomos_accesorios from '../assets/images/brands/Atomos_Accesorios.png';
+import angelbird_accesorios from '../assets/images/brands/Angelbird_Accesorios.png';
+import eartec_accesorios from '../assets/images/brands/Eartec_Accesorios.png';
+import unilumin_accesorios from '../assets/images/brands/Unilumin_Accesorios.png';
+import lacie_accesorios from '../assets/images/brands/Lacie_Accesorios.png';
+import swit_accesorios from '../assets/images/brands/Swit_Accesorios.png';
+import panasonic_audio from '../assets/images/brands/Panasonic_Audio.png';
+import shure_audio from '../assets/images/brands/Shure_Audio.png';
+import bose_audio from '../assets/images/brands/Bose_Audio.png';
+import roland_audio from '../assets/images/brands/Roland_Audio.png';
+import azden_audio from '../assets/images/brands/Azden_Audio.png';
+import blackmagic_interfaces from '../assets/images/brands/Blackmagic_Interfaces.png';
+import birddog_interfaces from '../assets/images/brands/BirdDog_Interfaces.png';
+import resi_interfaces from '../assets/images/brands/Resi_Interfaces.png';
+import wowza_interfaces from '../assets/images/brands/Wowza_Interfaces.png';
+import vegas_interfaces from '../assets/images/brands/Vegas_Interfaces.png';
+import telestream_interfaces from '../assets/images/brands/Telestream_Interfaces.png';
+import magix_interfaces from '../assets/images/brands/Magix_Interfaces.png';
+import reckreen_interfaces from '../assets/images/brands/Reckreen_Interfaces.png';
+import softron_interfaces from '../assets/images/brands/Softron_Interfaces.png';
+import yololiv_interfaces from '../assets/images/brands/Yololiv_Interfaces.png';
+
+
+
 
 export const equipmentData = {
     fotografiayvideo: { 
@@ -54,17 +60,17 @@ export const equipmentData = {
       icon: fotografiayvideoIcon,
       text: 'MARCAS ALIADAS',
       brands: [
-        {name: 'panasonic', image: panasonic, minicon:iconvideo },
-        {name: 'sony', image: sony, minicon:iconvideo},
-        {name: 'canon', image: canon, minicon:iconvideo},
-        {name: 'datavideo', image: datavideo, minicon:iconvideo},
-        {name: 'red', image: red, minicon:iconvideo},
-        {name: 'newTek', image: newtek, minicon:iconvideo},
-        {name: 'birdDog', image: birddog, minicon:iconvideo},
-        {name: 'jvc', image: jvc, minicon:iconvideo},
-        {name: 'aidaImaging', image: aidaimaging, minicon:iconvideo},
-        {name: 'optics', image: ptzoptics, minicon:iconvideo},
-        {name: 'datavideo', image: fujifilm, minicon:iconaccesorios},
+        {name: 'panasonic', image: panasonic},
+        {name: 'sony', image: sony},
+        {name: 'canon', image: canon},
+        {name: 'datavideo', image: datavideo},
+        {name: 'red', image: red},
+        {name: 'newTek', image: newtek},
+        {name: 'birdDog', image: birddog},
+        {name: 'jvc', image: jvc},
+        {name: 'aidaImaging', image: aidaimaging},
+        {name: 'optics', image: ptzoptics},
+        // {name: 'fujifilm', image: fujifilm},
       ]
     },
     accesorios: {
@@ -73,25 +79,25 @@ export const equipmentData = {
       icon: tripodeIcon,
       text: 'MARCAS ALIADAS',
       brands: [
-        {name: 'panasonic', image: sony, minicon:iconaccesorios },
-        {name: 'sony', image: canon, minicon:iconaccesorios},
-        {name: 'canon', image: blackmagic, minicon:iconaccesorios},
-        {name: 'datavideo', image: fujifilm, minicon:iconaccesorios},
-        {name: 'red', image: teradek, minicon:iconaccesorios},
-        {name: 'newTek', image: manfrotto, minicon:iconaccesorios},
-        {name: 'birdDog', image: acebil, minicon:iconaccesorios},
-        {name: 'jvc', image: roland, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: aja, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: canare, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: aja, minicon:iconaccesorios},
-        {name: 'optics', image: magewell, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: nanlite, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: atomos, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: angelbird, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: eartec, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: unilumin, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: lacie, minicon:iconaccesorios},
-        {name: 'aidaImaging', image: swit, minicon:iconaccesorios},
+        {name: 'sony_accesorios', image: sony_accesorios},
+        {name: 'canon_accesorios', image: canon_accesorios},
+        {name: 'blacmagic_accesorios', image: blacmagic_accesorios},
+        {name: 'fujifilm_accesorios', image: fujifilm_accesorios},
+        {name: 'teradek_accesorios', image: teradek_accesorios},
+        {name: 'manfrotto_accesorios', image: manfrotto_accesorios},
+        {name: 'aceail_accesorios', image: aceail_accesorios},
+        {name: 'roland_accesorios', image: roland_accesorios},
+        {name: 'aja_accesorios', image: aja_accesorios},
+        {name: 'canare_accesorios', image: canare_accesorios},
+        {name: 'magewell_accesorios', image: magewell_accesorios},
+        {name: 'nanlite_accesorios', image: nanlite_accesorios},
+        {name: 'atomos_accesorios', image: atomos_accesorios},
+        {name: 'angelbird_accesorios', image: angelbird_accesorios},
+        {name: 'eartec_accesorios', image: eartec_accesorios},
+        {name: 'unilumin_accesorios', image: unilumin_accesorios},
+        {name: 'lacie_accesorios', image: lacie_accesorios},
+        {name: 'swit_accesorios', image: swit_accesorios},
+
       ]
     },
     audio: {
@@ -100,11 +106,11 @@ export const equipmentData = {
         icon: audioIcon,
         text: 'MARCAS ALIADAS',
         brands: [
-          {name: 'panasonic', image: panasonic, minicon:iconaudio },
-          {name: 'sony', image: shure, minicon:iconaudio},
-          {name: 'canon', image: bose, minicon:iconaudio},
-          {name: 'datavideo', image: roland, minicon:iconaudio},
-          {name: 'red', image: azden, minicon:iconaudio},
+          {name: 'panasonic_audio', image: panasonic_audio},
+          {name: 'shure_audio', image: shure_audio},
+          {name: 'bose_audio', image: bose_audio},
+          {name: 'roland_audio', image: roland_audio},
+          {name: 'azden_audio', image: azden_audio},
         ]
     },
     software: {
@@ -113,16 +119,16 @@ export const equipmentData = {
         icon: softwareIcon,
         text: 'MARCAS ALIADAS',
         brands: [
-          {name: 'panasonic', image: blackmagic, minicon:iconsoftware },
-          {name: 'sony', image: birddog, minicon:iconsoftware},
-          {name: 'canon', image: resi, minicon:iconsoftware},
-          {name: 'datavideo', image: wowza, minicon:iconsoftware},
-          {name: 'red', image: vegas, minicon:iconsoftware},
-          {name: 'newTek', image: telestream, minicon:iconsoftware},
-          {name: 'birdDog', image: magix, minicon:iconsoftware},
-          {name: 'jvc', image: reckreen, minicon:iconsoftware},
-          {name: 'aidaImaging', image: softron, minicon:iconsoftware},
-          {name: 'optics', image: yololiv, minicon:iconsoftware},
+          {name: 'blackmagic_interfaces', image: blackmagic_interfaces },
+          {name: 'birddog_interfaces', image: birddog_interfaces},
+          {name: 'resi_interfaces', image: resi_interfaces},
+          {name: 'wowza_interfaces', image: wowza_interfaces},
+          {name: 'vegas_interfaces', image: vegas_interfaces},
+          {name: 'telestream_interfaces', image: telestream_interfaces},
+          {name: 'magix_interfaces', image: magix_interfaces},
+          {name: 'reckreen_interfaces', image: reckreen_interfaces},
+          {name: 'softron_interfaces', image: softron_interfaces},
+          {name: 'yololiv_interfaces', image: yololiv_interfaces},
         ]
     },  
 
